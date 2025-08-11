c58251f80df318c6e1a03ee4535269bc3aaf749e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-11 17:44:09 -0400

    Add Lesson_19_Introduction_to_Namespacing/part_2_myAppStructure.js

---
+++ Lesson_19_Introduction_to_Namespacing/part_2_myAppStructure.js
@@ -0,0 +1,11 @@
+const MyApp = {
+  propertyA: {},
+  propertyB: {},
+  myFunction() {
+    console.log("Hello from my function!");
+  },
+};
+MyApp.myFunction(); // Outputs: Hello from my function!
+MyApp.models = {};
+MyApp.views = {};
+MyApp.controllers = {};
