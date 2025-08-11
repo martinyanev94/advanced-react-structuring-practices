a3d936116ddb9c27771374f99844bbd3d0525e5b
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-11 17:46:13 -0400

    Add Lesson_5_Creational_Design_Patterns/part_3_singleton.js.js

---
+++ Lesson_5_Creational_Design_Patterns/part_3_singleton.js.js
@@ -0,0 +1,28 @@
+class Singleton {
+    constructor() {
+        if (Singleton.instance) {
+            return Singleton.instance;
+        }
+        Singleton.instance = this;
+        this.data = [];
+    }
+
+    addData(item) {
+        this.data.push(item);
+    }
+
+    getData() {
+        return this.data;
+    }
+}
+
+// Usage
+const singleton1 = new Singleton();
+singleton1.addData('First Item');
+
+const singleton2 = new Singleton();
+singleton2.addData('Second Item');
+
+console.log(singleton1.getData()); // Outputs: ['First Item', 'Second Item']
+console.log(singleton2.getData()); // Outputs: ['First Item', 'Second Item']
+console.log(singleton1 === singleton2); // Outputs: true
