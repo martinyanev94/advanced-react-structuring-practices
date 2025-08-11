fb5c2499ea28c154b5f7a1c3e76304a60156ae95
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-11 17:44:21 -0400

    Add Lesson_1_Introduction_to_Modern_JavaScript/part_4_greetingMessage.js

---
+++ Lesson_1_Introduction_to_Modern_JavaScript/part_4_greetingMessage.js
@@ -0,0 +1,3 @@
+const name = 'Alice';
+const greeting = `Hello, ${name}. Welcome to the modern JavaScript tutorial!`;
+console.log(greeting); // Hello, Alice. Welcome to the modern JavaScript tutorial!
