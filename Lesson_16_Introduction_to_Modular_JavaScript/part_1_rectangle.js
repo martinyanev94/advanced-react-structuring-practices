b5ade8afca2c273e53f3f5a33c3823d488f4dbd7
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-11 17:43:45 -0400

    Add Lesson_16_Introduction_to_Modular_JavaScript/part_1_rectangle.js

---
+++ Lesson_16_Introduction_to_Modular_JavaScript/part_1_rectangle.js
@@ -0,0 +1,8 @@
+// rectangle.js
+export function calculateArea(length, width) {
+    return length * width;
+}
+
+export function calculatePerimeter(length, width) {
+    return 2 * (length + width);
+}
